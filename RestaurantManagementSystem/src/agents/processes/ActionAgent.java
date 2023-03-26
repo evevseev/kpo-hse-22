9b5cd6952d6c66fe667f3eead3eadf9e2f0fd806
Dmitry-Rosinskiy <118693256+Dmitry-Rosinskiy@users.noreply.github.com>
2023-03-26 18:43:10 +0300

    Updated agent files

--- RestaurantManagementSystem/src/agents/processes/ActionAgent.java
+++ RestaurantManagementSystem/src/agents/processes/ActionAgent.java
@@ -1,5 +1,7 @@
 package agents.processes;
 
-public class ActionAgent {
+import agents.BasicAgent;
+
+public class ActionAgent extends BasicAgent {
     // запрашивает управляющего агента зарезервировать агента повара и агента оборудования для выполнения операции процесса.
 }
